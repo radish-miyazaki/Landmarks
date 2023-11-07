8c3552a7d1eb85e685edabf37bc138ed80b4a877
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2023-11-07 15:46:03 +0900

    building lists and navigation

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+struct LandmarkList: View {
+    @State private var columnVisibility = NavigationSplitViewVisibility.all
+    
+    var body: some View {
+        NavigationSplitView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a landmark")
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
