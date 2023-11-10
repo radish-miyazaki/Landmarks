a735ff3e404f57a154219f6bd07d1730367b6fd8
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2023-11-10 15:30:50 +0900

    working with UI controls

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -4,14 +4,13 @@ import Foundation
 class ModelData {
     var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
-    
+    var profile = Profile.default
     var categories: [String: [Landmark]] {
         Dictionary(
             grouping: landmarks,
             by: { $0.category.rawValue }
         )
     }
-    
     var features: [Landmark] {
         landmarks.filter { $0.isFeatured }
     }
---
+++ Landmarks/Views/Profiles/ProfileSummary.swift
@@ -0,0 +1,52 @@
+import SwiftUI
+
+struct ProfileSummary: View {
+    @Environment(ModelData.self) var modelData
+    var profile: Profile
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(profile.username)
+                    .bold()
+                    .font(.title)
+                
+                Text("Notifications: \(profile.preferNotifications ? "On": "Off")")
+                Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
+                Text("Goal Date ") + Text(profile.goalDate, style: .date)
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Completed Badge")
+                        .font(.headline)
+                    
+                    ScrollView(.horizontal) {
+                        HStack {
+                            HikeBadge(name: "First Hike")
+                            HikeBadge(name: "Earth day")
+                                .hueRotation(Angle(degrees: 90))
+                            HikeBadge(name: "Tenth Hike")
+                                .grayscale(0.5)
+                                .hueRotation(Angle(degrees: 45))
+                        }
+                        .padding(.bottom)
+                    }
+                }
+                
+                Divider()
+                
+                VStack {
+                    Text("Recent Hikes")
+                        .font(.headline)
+                    HikeView(hike: modelData.hikes[0])
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ProfileSummary(profile: Profile.default)
+        .environment(ModelData())
+}
